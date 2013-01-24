b9eec4e2a9ea29cda1d0ee20f2b6432ec1cb7e13
Todd Stout <todd.tstout@gmail.com>
2013-01-23 20:47:30 -0600

    update serialization example to actually read/write to/from a file

--- protobuf/src/test/java/protobuf/example/SuperHeroTest.java
+++ protobuf/src/test/java/protobuf/example/SuperHeroTest.java
@@ -1,19 +1,26 @@
 package protobuf.example;
 
-import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.common.io.Files;
+import com.google.protobuf.Message;
 import org.junit.Test;
 
-import static junit.framework.Assert.assertEquals;
-import static protobuf.example.SuperHero.*;
+import java.io.File;
+import java.io.IOException;
+
+import static junit.framework.Assert.*;
 import static protobuf.example.SuperHero.FutileSuperpowerType.*;
+import static protobuf.example.SuperHero.*;
 
 public class SuperHeroTest {
 
     static final double LAT = 55.949354;
     static final double LONG = -3.147583;
 
+    File dataFile = new File(System.getProperty("user.home"), "sighting.dat");
+
+
     @Test
-    public void serializeSuperHeroSighting() throws InvalidProtocolBufferException {
+    public void serializeSuperHeroSighting() throws IOException {
 
         UselessSuperHeroSighting sighting = UselessSuperHeroSighting.newBuilder()
                 .setLatitude(LAT)
@@ -22,12 +29,21 @@ public class SuperHeroTest {
                 .setPowerType(COMMUNICATE_WITH_FRUIT)
                 .build();
 
-        byte[] serializedSighting = sighting.toByteArray();
+        serializeToFile(sighting);
 
-        UselessSuperHeroSighting hydratedSighting = UselessSuperHeroSighting.parseFrom(serializedSighting);
+        UselessSuperHeroSighting hydratedSighting = deserialzeFromFile();
 
         assertEquals(hydratedSighting.getLatitude(), LAT);
         assertEquals(hydratedSighting.getLongitude(), LONG);
         assertEquals(hydratedSighting.getPowerType(), COMMUNICATE_WITH_FRUIT);
     }
+
+    void serializeToFile(Message message) throws IOException {
+        Files.write(message.toByteArray(), dataFile);
+    }
+
+    UselessSuperHeroSighting deserialzeFromFile() throws IOException {
+        return UselessSuperHeroSighting.parseFrom(Files.toByteArray(dataFile));
+    }
+
 }

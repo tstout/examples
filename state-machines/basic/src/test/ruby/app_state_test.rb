76bc955c0675a6d4253188d5f5e748a987ad9d35
Todd Stout <todd.tstout@gmail.com>
2013-12-24 22:23:13 -0600

    more gradle cleanup

--- state-machines/basic/src/test/ruby/app_state_test.rb
+++ state-machines/basic/src/test/ruby/app_state_test.rb
@@ -3,23 +3,16 @@ require 'minitest/autorun'
 
 class AppStateTest < MiniTest::Test
 
-  # Called before every test method runs. Can be used
-  # to set up fixture information.
   def setup
-    # Do nothing
   end
 
-  # Called after every test method runs. Can be used to tear
-  # down fixture information.
-
   def teardown
-    # Do nothing
   end
 
   # Fake test
   def test_fail
 
-    # To change this template use File | Settings | File Templates.
-    fail('Not implemented')
+    skip
+    #fail('Not implemented')
   end
 end
